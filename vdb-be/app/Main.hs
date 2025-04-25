88e1c7efcaa71f262771d19bcb301d2438b02556
h-alice <admin@halice.art>
2025-04-26 01:40:43 +0800

    Variables.

--- vdb-be/app/Main.hs
+++ vdb-be/app/Main.hs
@@ -15,6 +15,19 @@ import Reranker.Client
 import Data.List (sortBy)
 import Data.Ord (comparing)
 
+
+-- Constants
+-- | The API endpoint for the Weaviate vector database.
+-- NOTE: Replace with flexible variable such as environment variable.
+weaviateEndpoint :: String
+weaviateEndpoint = "http://localhost:8080/v1/graphql" -- Adjust if your Weaviate runs elsewhere
+
+-- | The API endpoint for the Reranker service.
+-- NOTE: Replace with flexible variable such as environment variable.
+rerankerEndpoint :: String
+rerankerEndpoint = "http://127.0.0.1:8089/v1/rerank"
+
+
 -- Example Usage (main function)
 main :: IO ()
 main = do
@@ -24,14 +37,13 @@ main = do
     putStrLn "Fetching documents from Weaviate vector database..."
 
     let query =  WQ.mkHybridQuery "Document" >>= WQ.setLimit 15 >>= WQ.setAlpha 0.45 >>= WQ.setUserPrompt mainQuery
-    let weaviateEndpoint = "http://localhost:8080/v1/graphql" -- Adjust if your Weaviate runs elsewhere
     searchResult <- WC.weaviateHybridSearch weaviateEndpoint query
     
     putStrLn "Reranking documents..."
     case getDocuments searchResult of
         Left err -> putStrLn $ "Error fetching documents: " ++ err
         Right decodedResponse -> do
-            rerankRes <- rerankDocuments "http://127.0.0.1:8089/v1/rerank" mainQuery 1 decodedResponse
+            rerankRes <- rerankDocuments rerankerEndpoint mainQuery 1 decodedResponse
             case rerankRes of
                 Left err -> putStrLn $ "Error: " ++ err
                 Right rerankedResults -> do
